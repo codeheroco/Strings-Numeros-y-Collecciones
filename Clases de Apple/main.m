855d0a1b6c1d4e4be76d31aaaf1db2400b82cda1
Oscar Vicente González Greco <oscar@Gonzalezs-iMac.local>
2013-07-28 18:42:21 -0430

    Initial Commit

---
+++ Clases de Apple/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Clases de Apple
+//
+//  Created by Oscar Vicente González Greco on 28/7/13.
+//  Copyright (c) 2013 Codehero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
